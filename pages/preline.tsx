6519ea63353e3e1caef637cc7d415c6b67d16f2b
takunagai <tak_na@icloud.com>
2022-07-02 22:29:16 +0900

    カード(横並び)の追加

--- pages/preline.tsx
+++ pages/preline.tsx
@@ -263,6 +263,53 @@ const Preline: NextPage = () => {
             </section>
             <hr className="my-10" />
             <section>
+                <div className="bg-white border rounded-xl shadow-sm sm:flex dark:bg-gray-800 dark:border-gray-700 dark:shadow-slate-700/[.7]">
+                  <div className="flex-shrink-0 relative w-full rounded-t-xl overflow-hidden pt-[40%] sm:rounded-l-xl sm:max-w-[15rem] md:rounded-tr-none md:max-w-xs">
+                    <Image
+                        src="https://picsum.photos/id/1/600/400.webp"
+                        width={600}
+                        height={400}
+                        alt="代替テキスト"
+                        className="w-full h-full absolute top-0 left-0 object-cover"
+                    />
+                  </div>
+                  <div className="flex flex-wrap">
+                    <div className="p-4 flex flex-col h-full sm:p-7">
+                      <h3 className="text-lg font-bold text-gray-800 dark:text-white">Card title</h3>
+                      <p className="mt-1 text-gray-800 dark:text-gray-400">
+                        Some quick example text to build on the card title and make up the bulk of the cards content.
+                      </p>
+                      <div className="mt-5 sm:mt-auto">
+                        <p className="text-xs text-gray-500 dark:text-gray-500">
+                          Last updated 5 mins ago
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+            </section>
+            <hr className="my-10" />
+            <section>
+                <div className="bg-yellow-50 border border-yellow-200 rounded-md p-4" role="alert">
+                  <div className="flex">
+                    <div className="flex-shrink-0">
+                      <svg className="h-4 w-4 text-yellow-400 mt-0.5" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
+                        <path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
+                      </svg>
+                    </div>
+                    <div className="ml-4">
+                      <h3 className="text-sm text-yellow-800 font-semibold">
+                        Cannot connect to the database
+                      </h3>
+                      <div className="mt-1 text-sm text-yellow-700">
+                        We are unable to save any progress at this time.
+                      </div>
+                    </div>
+                  </div>
+                </div>
+            </section>
+            <hr className="my-10" />
+            <section>
 
             </section>
         </Layout>
