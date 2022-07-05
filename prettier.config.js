32d93da6e1a103ca1b804931534c326c0447fac1
takunagai <tak_na@icloud.com>
2022-07-06 08:51:31 +0900

    prettier, prettier-plugin-tailwind の導入

---
+++ prettier.config.js
@@ -0,0 +1,5 @@
+// prettier-plugin-tailwind (prettier-plugin-svelte との併用は不可)
+module.exports = {
+  plugins: [require("prettier-plugin-tailwindcss")],
+  // tailwindConfig: './styles/tailwind.config.js', // Default: 同ディレクトリの tailwind.config.js
+};
