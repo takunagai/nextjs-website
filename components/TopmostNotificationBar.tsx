23addd9665cd1ac9aebdfecb60d99dd9a8b3d1ea
takunagai <tak_na@icloud.com>
2022-07-08 23:28:52 +0900

    ページ最上部のお知らせの文字サイズを調整

--- components/TopmostNotificationBar.tsx
+++ components/TopmostNotificationBar.tsx
@@ -6,7 +6,7 @@ const TopmostNotificationBar = () => {
     <aside className="bg-gradient-to-r from-purple-500 to-pink-500 py-3">
       <div className="flex flex-col justify-center text-center md:flex-row">
         <FlagIcon className="mx-auto inline h-7 w-7 text-white md:mx-0" />
-        <p className="mx-3 inline-block text-lg font-bold">
+        <p className="mx-3 inline-block font-bold">
           目立たせたいお知らせが入ります！
         </p>
         <span className="mx-auto inline-block rounded-full bg-white py-1.5 px-3 text-xs font-bold text-pink-500 md:mx-0">
