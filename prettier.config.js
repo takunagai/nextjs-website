9e6194e11d0562b6d91ac4e24406fe3477342179
takunagai <tak_na@icloud.com>
2022-07-25 11:09:23 +0900

    Prettier の設定 (末尾コンマ付ける、Tab 2スペース、セミコロン付けない)

--- prettier.config.js
+++ prettier.config.js
@@ -1,5 +1,8 @@
 // prettier-plugin-tailwind (prettier-plugin-svelte との併用は不可)
 module.exports = {
+  trailingComma: "all",
+  tabWidth: 2,
+  semi: false,
   plugins: [require("prettier-plugin-tailwindcss")],
   // tailwindConfig: './styles/tailwind.config.js', // Default: 同ディレクトリの tailwind.config.js
-};
+}
