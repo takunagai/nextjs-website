f3878e9cb5cce801ecec34ab6a9ae444e91fca0b
takna <tak_na@icloud.com>
2022-08-14 09:37:01 +0900

    不要な await を削除、コメントの整形

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,6 +3,7 @@
  * <html>, <body> タグの定義を行うコンポーネント
  * https://qiita.com/Yuki-Kurita/items/6a0eae00999e1294a3b1
  * https://stackoverflow.com/questions/67087999/how-to-properly-type-the-document-tsx-file-from-next-js
+ * 元コード: Learn Next.js (公式)
  *
  * 注意：
  * サーバサイドでのみ実行。クライアントサイドの処理(onClickのようなイベントハンドラなど)を書かないこと
@@ -10,7 +11,6 @@
  *    アプリケーションのロジックや、styled-jsx の ような CSS の設定は、_app.tsx に書く
  * DocumentのgetInitialProps は、クライアントサイドの遷移中、ページが静的に最適化されている場合にも呼び出さない
  */
-// Learn Next.js (公式) より
 import Document, {
   DocumentContext,
   Head,
@@ -21,7 +21,7 @@ import Document, {
 
 class MyDocument extends Document {
   static async getInitialProps(ctx: DocumentContext) {
-    return await Document.getInitialProps(ctx)
+    return Document.getInitialProps(ctx)
   }
 
   render() {
