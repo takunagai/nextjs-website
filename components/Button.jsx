a98c994ce03e7c075dc4c9de031a633126d8ef32
takna <tak_na@icloud.com>
2022-08-19 09:39:37 +0900

    ボタンコンポーネントを TypeScript に

--- components/Button.jsx
+++
@@ -1,9 +0,0 @@
-import styles from "./Button.module.css"
-
-export default function Button() {
-  return (
-    <button type="button" className={styles.error}>
-      CSS Modules でスタイリングしたコンポーネント
-    </button>
-  )
-}
