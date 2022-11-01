54c1929f80c69ae2f0b7c06b0a57687ffdd7d3fe
takna <tak_na@icloud.com>
2022-11-02 07:59:10 +0900

    images.domains に microCMS API のドメインを追加

--- next.config.js
+++ next.config.js
@@ -5,6 +5,10 @@ module.exports = {
 // URL のホスト名を images.domains 構成に追加
 module.exports = {
   images: {
-    domains: ["picsum.photos", "nextjs-website-8rsxjy86c-takunagai.vercel.app"],
+    domains: [
+      "picsum.photos",
+      "images.microcms-assets.io",
+      "nextjs-website-8rsxjy86c-takunagai.vercel.app",
+    ],
   },
 }
