67b9a0b5c01f9aeac36df32cb28b6d524ab6a11f
takna <tak_na@icloud.com>
2022-11-07 16:24:27 +0900

    Date コンポーネントを調整

--- components/date.tsx
+++ components/date.tsx
@@ -1,12 +1,20 @@
 /**
- * 日付を整形する
+ * date-fns で日付を整形し表示するコンポーネント
+ * Usage: Import & `<Date dateString={タイムスタンプ文字列} />`
+ * @ref https://date-fns.org/
+ * @ref https://qiita.com/FaLiP_lvsg/items/1bed7344763d01442c92
  */
 import { parseISO, format } from "date-fns"
+import ja from "date-fns/locale/ja"
 
 type Props = {
   dateString: string
 }
 export default function Date({ dateString }: Props) {
   const date = parseISO(dateString)
-  return <time dateTime={dateString}>{format(date, "yyyy年M月d日")}</time>
+  return (
+    <time dateTime={dateString}>
+      {format(date, "yyyy年MM月dd日", { locale: ja })} {/* (eee) で曜日*/}
+    </time>
+  )
 }
