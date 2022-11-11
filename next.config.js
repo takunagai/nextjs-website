299f8e281b9cf571209fc4bb884db9cb26bcb58e
takna <tak_na@icloud.com>
2022-11-11 20:41:55 +0900

    next.config.js で、画像最適化を無効化

--- next.config.js
+++ next.config.js
@@ -1,14 +1,16 @@
 module.exports = {
   reactStrictMode: true,
-}
-
-// URL のホスト名を images.domains 構成に追加
-module.exports = {
   images: {
+    // URL のホスト名を images.domains 構成に追加
     domains: [
       "picsum.photos",
       "images.microcms-assets.io",
-      "nextjs-website-8rsxjy86c-takunagai.vercel.app",
+      "vercel.app",
+      "hanshin-branch.org",
     ],
+    unoptimized: true, // 画像最適化の無効化。Vercel なら不要
+    // https://nextjs.org/docs/messages/export-image-api
   },
 }
+
+module.exports = {}
