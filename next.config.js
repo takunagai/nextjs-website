8ab20f09a00367035d34ce636b52a81cd6bd0d44
takna <tak_na@icloud.com>
2022-11-12 05:53:03 +0900

    next.config.js で画像最適化をオフに

--- next.config.js
+++ next.config.js
@@ -1,10 +1,8 @@
+// URL のホスト名を images.domains 構成に追加 & Vercel 以外なら画像最適化をオフ
 module.exports = {
   reactStrictMode: true,
-}
-
-// URL のホスト名を images.domains 構成に追加
-module.exports = {
   images: {
     domains: ["picsum.photos"],
   },
+  unoptimized: true,
 }
